dcdb1f7c2b0b29453a7febc7f584b6f8fa63d655
Codringtoncoding <humphrey.codrington@gmail.com>
2020-07-09 15:34:19 +0100

    reading the csv file

--- CSVreader.cs
+++ CSVreader.cs
@@ -1,12 +1,25 @@
+using System.Data;
 using System;
+using System.IO;
+
 
 namespace supportbank
 {
 
     public class CSVreader
+
     {
-
-    }
+        static void Main(string[] args)
+            {
+                String csv = File.ReadAllText("Transactions2014.csv");
+                    Console.WriteLine(csv);
     
-
+            }
+        
+        public static int add(int value1,int value2)
+        {
+            return value1 + value2;
+        }
+        
+    }
 }
