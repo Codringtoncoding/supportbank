18ddca188b18911eea1dc4f8b854679fc72e9d7e
Codringtoncoding <humphrey.codrington@gmail.com>
2020-07-09 12:18:53 +0100

    start of supportbank project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace supportbank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
