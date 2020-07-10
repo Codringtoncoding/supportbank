fc6daaa72602a1f0f0ea8912ec7e092b54c7f310
Codringtoncoding <humphrey.codrington@gmail.com>
2020-07-10 15:37:48 +0100

    app working working but not revealing the correct answers

--- Account.cs
+++ Account.cs
@@ -1,10 +1,42 @@
 using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace supportbank
 {
     
-    public class Employee
+    public class Account
     {
+        public string Name;
+
+        public List<Transaction> Owed;
+
+        public List<Transaction> Owe;
+
+        public Account(string name)
+        {
+            Name = name;
+            Owed = new List<Transaction>();
+            Owe = new List<Transaction>();
+
+        }
+        public decimal GetTotalAmount()
+        {
+            var totalOwed = new Decimal(0);
+            foreach(var currentOwedTransaction in Owed)
+            {
+                totalOwed = totalOwed + currentOwedTransaction.amount;
+            }
+            foreach(var currentOweTransaction in Owe)
+            {
+                totalOwed = totalOwed - currentOweTransaction.amount;
+            }
+
+            return totalOwed;
+            
+            }
     }
 
 }
--- program.cs
+++ program.cs
@@ -1,5 +1,6 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace supportbank
 {
     
@@ -7,7 +8,40 @@ namespace supportbank
     {
         static void Main(string[] args)
         {
-            CSVreader.ReadTransactionsFromfile();
+           var transactions = CSVreader.ReadTransactionsFromfile();
+           Console.WriteLine(transactions);
+
+           var accounts = GetAccounts(transactions);
+
+           Console.WriteLine(accounts);
+
+        }
+
+        static List<Account> GetAccounts(List<Transaction> transactions)
+        {
+          var names = getNames(transactions); 
+          var allAccounts = new List<Account>();
+
+          foreach(var currentName in names)
+          {  
+              var currentAccount = new Account(currentName);
+              allAccounts.Add(currentAccount);
+          }
+
+          return allAccounts;
+        }
+
+        static HashSet<string> getNames(List<Transaction> transactions)
+        {
+            var allNames = new HashSet<string>();
+
+            foreach(var currentTransaction in transactions)
+            {
+                allNames.Add(currentTransaction.to);
+                allNames.Add(currentTransaction.from);
+            }
+
+            return allNames;
         }
     }
 
