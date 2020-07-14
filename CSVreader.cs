aa3254de5fb271f1f7be9651fc00e884468bc53b
Codringtoncoding <humphrey.codrington@gmail.com>
2020-07-14 15:10:14 +0100

    adding logger to the file

--- CSVreader.cs
+++ CSVreader.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Globalization;
+using NLog;
 
 namespace supportbank
 {
@@ -11,8 +12,12 @@ namespace supportbank
     public class CSVreader
 
     {
-        public static List<Transaction> ReadTransactionsFromfile()
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+        public static List<Transaction> ReadTransactionsFromfile
+        {
+            get
             {
+                logger.Info("About to read transactions");
                 var transactions = new List<Transaction>();
 
                 var csv = File.ReadAllLines("DodgyTransactions2015.csv").Skip(1);
@@ -22,30 +27,32 @@ namespace supportbank
                     var parts = line.Split(",");
 
 
-                    var date = DateTime.Parse(parts[0],new CultureInfo("en-GB"));
+                    var date = DateTime.Parse(parts[0], new CultureInfo("en-GB"));
 
                     var from = parts[1];
                     var to = parts[2];
                     var narrative = parts[3];
 
-                    try{
-                            var amount = Convert.ToDecimal(parts[4]);
-                            var mytransaction = new Transaction(date,to,from,narrative,amount);
-                    transactions.Add(mytransaction);
+                    try
+                    {
+                        var amount = Convert.ToDecimal(parts[4]);
+                        var mytransaction = new Transaction(date, to, from, narrative, amount);
+                        transactions.Add(mytransaction);
                     }
                     catch (FormatException mistake)
                     {
-                            Console.WriteLine($"you enterered {parts[4]} which is an invalid format");
-                            throw mistake;
-                     
-                    
+                        Console.WriteLine($"you enterered {parts[4]} which is an invalid format");
+                        throw mistake;
+
+
                     }
 
-                   
-                 
+
+
                 }
 
                 return transactions;
             }
+        }
     }
 }
--- program.cs
+++ program.cs
@@ -1,13 +1,30 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System;
 using System.Collections.Generic;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace supportbank
 {
         class Program
     {
+        private static void ConfigureLogger()
+        {
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"SupportBank.log", Layout = @"${longdate}) -${logger} - ${logger}: ${message}"};
+            config.AddTarget("File Logger", target);
+
+            var consoleTarget = new ConsoleTarget { Layout =@"${longdate}) -${logger} - ${logger}: ${message}"};
+            config.AddTarget("Console Logger", consoleTarget);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug,target));
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Error, consoleTarget));
+            LogManager.Configuration = config;
+        }
         static void Main(string[] args)
         {
-           var transactions = CSVreader.ReadTransactionsFromfile();
+           var transactions = CSVreader.ReadTransactionsFromfile;
        
 
 
